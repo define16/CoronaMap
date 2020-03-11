6cfb8abd322c83d4ccea0ef43b935ae407a4fbe8
define16 <radix7318@gmail.com>
2020-03-12 01:50:09 +0900

    Apply DB (6) 기능: 이동수단을 색상으로 웹의 지도 위에 출력  * 아직 개발중..  * index.html 의 271번 알아내기    - JS의 Map의 Value가 List인 경우 특정원소를 불러오는 에러 수정

--- coronaMap/mainsite/views.py
+++ coronaMap/mainsite/views.py
@@ -25,36 +25,35 @@ def index(request):
     """
     results_map = {}
     person_map = {}
-    total_person_num: int = 1
+    check_person_num: int = 1
     total_spot_cnt: int = 1
     spot_cnt:int = 0
     check_date: int = 0
     change_date:int = 0
-
     results_transportation_map = {}
 
     results = InfectedPeople.objects.all().order_by('id') # -는 내림차순
     # 커스텀 오버레이, 날짜별 방문 장소를 분리
     for result in results:
-        if result.person_num != total_person_num or len(results) == total_spot_cnt: # 사람별로 구분
-            results_map["person_" + str(total_person_num)] = person_map
+        if result.person_num != check_person_num or len(results) == total_spot_cnt: # 사람별로 구분
+            results_map["person_" + str(check_person_num)] = person_map
             spot_cnt = 0
             person_map = {}
 
-        if result.person_num == total_person_num :
-            tmp_transportation = result.transportation
-            color = convert_color(tmp_transportation)
-            if int(result.visited_date) != check_date :
-                change_date = 1 # 날짜가 바뀌었을 때
-            else :
-                change_date = 0; # 날짜가 안 바뀌었을 때(즉, 같은날 이동)
-            spot_map = {'person_num' : result.person_num, 'region': result.region ,'visited_date': result.visited_date,'address': result.address,'place': result.place,
-                        'latitude': result.latitude,'longitude': result.longitude,'transportation': result.transportation,'color': color, 'change_date' : change_date}
-            person_map[str(spot_cnt)] = spot_map
-            spot_cnt+=1
-            check_date = result.visited_date
-
-        total_person_num = result.person_num
+        tmp_transportation = result.transportation
+        color = convert_color(tmp_transportation)
+        if int(result.visited_date) != check_date :
+            change_date = 1 # 날짜가 바뀌었을 때
+        else :
+            change_date = 0; # 날짜가 안 바뀌었을 때(즉, 같은날 이동)
+        spot_map = {'person_num' : result.person_num, 'region': result.region ,'visited_date': result.visited_date,
+                    'address': result.address,'place': result.place, 'latitude': result.latitude,
+                    'longitude': result.longitude,'transportation': result.transportation,'color': color,
+                    'change_date' : change_date}
+        person_map[str(spot_cnt)] = spot_map
+        spot_cnt+=1
+        check_date = result.visited_date
+        check_person_num = result.person_num #
         total_spot_cnt += 1
 
     total_person_cnt = len(results_map)
@@ -73,58 +72,53 @@ def index(request):
         transport_idx = 0
         path_idx = 0
         for result in results.values(): # 날짜 + 이동수단로 구분
-            # print("result : " , result)
-            # print("visited_date : " , result.get("visited_date"), date_tmp)
-            # print("transportation : " , result.get("transportation"), transport_tmp)
-            if result.get("visited_date") == date_tmp or not path_map: # 날짜별로 구분
-                if transport_tmp == "" : # 이동수단별로 구분
+            if result.get("visited_date") == date_tmp or not path_map:
+                # 날짜별로 구분
+                if transport_tmp == "" or result.get("transportation") == transport_tmp : # 같은 이동수단으로 다음 장소로 이동한 경우
                     latitude_before = result.get("latitude")
                     longitude_before = result.get("longitude")
-                    spot_map = {"latitude": result.get("latitude"), "longitude": result.get("longitude")}
+                    # TODO : 반복[1] path_append(key, lat, log) => return path_map
+                    spot_map = {"latitude": result.get("latitude"), "longitude": result.get("longitude"), "color" : convert_color(result.get("transportation"))}
                     path_map[str(path_idx)] = spot_map
-                    # print("path_map00 : " , path_map)
-                    path_idx += 1
-                elif result.get("transportation") == transport_tmp : # 같은 이동수단으로 다음 장소로 이동한 경우
-                    latitude_before = result.get("latitude")
-                    longitude_before = result.get("longitude")
-                    spot_map = {"latitude": result.get("latitude"), "longitude": result.get("longitude")}
-                    path_map[str(path_idx)] = spot_map
-                    # print("path_map01 : " , path_map)
                     path_idx += 1
                 else : # 이동수단을 변경한 경우
-                    path_map["color"] = convert_color(transport_tmp)
-                    # print("path_map02 : " , path_map)
-                    transport_map["transport"+str(transport_idx)] = path_map
+                    transport_map["transport"+str(transport_idx)] = path_map # 현 이동수단으로 움직인 경로를 저장
                     transport_idx += 1
-                    path_map = {}
+                    path_map = {} # 경로 초기화
                     path_idx = 0
-                    spot_map = {"latitude": latitude_before, "longitude": longitude_before}
+                    # TODO : 반복[1] path_append(key, lat, log) => return path_map
+                    spot_map = {"latitude": latitude_before, "longitude": longitude_before, "color" : convert_color(transport_tmp)}
                     path_map[str(path_idx)] = spot_map
                     path_idx += 1
-                    spot_map = {"latitude": result.get("latitude"), "longitude": result.get("longitude")}
+                    # TODO : 반복[1] path_append(key, lat, log) => return path_map
+                    spot_map = {"latitude": result.get("latitude"), "longitude": result.get("longitude"), "color" : convert_color(result.get("transportation"))}
                     path_map[str(path_idx)] = spot_map
                     path_idx += 1
             else:
-                # print(transport_map)
                 # 하루에 하나의 장소만 간 경우 또는 하나의 이동수단만 이용한 경우
                 if not transport_map or len(transport_map) == 1:
-                    path_map["color"] = convert_color(result.get("transportation"))
                     transport_map["transport"+str(transport_idx)] = path_map
+
                     path_map = {}
 
                 date_map[str(date_tmp)] = transport_map
                 transport_map = {}
                 transport_idx = 0
+                path_idx = 0
                 latitude_before = result.get("latitude")
                 longitude_before = result.get("longitude")
-                spot_map = {"latitude": result.get("latitude"), "longitude": result.get("longitude")}
+                # TODO : 반복[1] path_append(key, lat, log) => return path_map
+                spot_map = {"latitude": result.get("latitude"), "longitude": result.get("longitude"), "color" : convert_color(result.get("transportation"))}
                 path_map[str(path_idx)] = spot_map
                 path_idx += 1
 
             transport_tmp = result.get("transportation") # 이전(index-1) 이동수단
             date_tmp = result.get("visited_date") # 이전(index-1) 날짜
-        print(key)
-        print(date_map)
+
+
+        transport_map["transport" + str(transport_idx)] = path_map
+
+        date_map[str(date_tmp)] = transport_map # 마지막 날짜를 저장
         results_transportation_map[key] = date_map
 
     # print(total_person_cnt)
@@ -146,22 +140,17 @@ def index(request):
 
     print(len(results_transportation_map))
     for key,results in results_transportation_map.items() :
-        print(key)
-        print(results)
-        # for result in results.values():
-        #     print(result)
-
-        #     print("person_num : " + str(result.get("person_num")))
-        #     print("region : " + result.get("region"))
-        #     print("visited_date : " + str(result.get("visited_date")))
-        #     print("address : " + result.get("address"))
-        #     print("place : " + result.get("place"))
-        #     print("latitude : " + result.get("latitude"))
-        #     print("longitude : " + result.get("longitude"))
-        #     print("transportation : " + result.get("transportation"))
-        #     print("color : " + result.get("color"))
-        #     print("change_date : " + str(result.get("change_date")))
-        #     print()
+        print("["+key+"]")
+        for date,result in results.items():
+            print("["+date+"]")
+            for k1, path in result.items() :
+                print("[change Transport]", k1)
+                for k2, spot in path.items():
+                    print("key :", k2)
+                    print("latitude :", spot.get('latitude'))
+                    print("longitude :", spot.get('longitude'))
+                    print("color :", spot.get('color'))
+                    print()
 
     return render(request, 'index.html', {'api_key' : kakao_api, 'total_person_cnt' : total_person_cnt ,  'results_map'
     : results_map, 'results_transportation_map' : results_transportation_map})
@@ -214,7 +203,5 @@ def search():
 
 def convert_color(transportation) :
     if transportation in colour_template.keys():
-        color = colour_template.get(transportation)
-    else:
-        color = BLACK
-    return color
+        return colour_template.get(transportation)
+    return BLACK
